09a256651a1880c7a880edee2534cf7af6482724
zhengjm <284655785@qq.com>
2020-08-19 17:04:22 +0800

    modify id

--- model/baseModel.go
+++ model/baseModel.go
@@ -3,7 +3,7 @@ package model
 import "time"
 
 type BaseModel struct {
-	ID        int64     `gorm:"primary_key" json:"-"`
+	ID        int64     `gorm:"primary_key" json:"id"`
 	CreatedAt time.Time `gorm:"column:created_at" json:"created_at"`
 	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at"`
 	//Deleted     bool      `json:"deleted"`
